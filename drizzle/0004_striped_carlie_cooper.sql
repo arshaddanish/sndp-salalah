ab065977d5e27d3321a612e009c485242bfa5c7f
Jyothsna63 <jyothsnapanchireddy@gmail.com>
2026-04-10 20:35:47 +0530

    fix: rename migration to 0005 and fix duplicate RENAME and DROP INDEX

--- drizzle/0004_striped_carlie_cooper.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "members" RENAME COLUMN "union" TO "union_name";--> statement-breakpoint
-DROP INDEX "transaction_categories_name_trgm_idx";--> statement-breakpoint
-DROP INDEX "transaction_categories_name_unique";--> statement-breakpoint
-CREATE UNIQUE INDEX "transaction_categories_name_unique" ON "transaction_categories" USING btree (lower("name"));
