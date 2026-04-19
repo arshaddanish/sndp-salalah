addaaede9ea81b91517552cba6105883e3a10893
Arshad Danish <arshaddanishthana@gmail.com>
2026-04-19 22:35:41 +0530

    fix: fix build error

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,9 +1,7 @@
 import { createAuthClient } from 'better-auth/react';
 import { usernameClient } from 'better-auth/client/plugins';
 
-import { env } from '@/lib/env';
-
 export const authClient = createAuthClient({
-  baseURL: env.BETTER_AUTH_URL,
+  baseURL: process.env['NEXT_PUBLIC_BETTER_AUTH_URL'],
   plugins: [usernameClient()],
 });
